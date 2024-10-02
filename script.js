b7a43b7c9fc604e25e116a43b753e0e6330dd312
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-02 20:46:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var board = [ , , , , , , , , ];
-var turn = "X";
+let board = [ , , , , , , , , ];
+let turn = "X";
+let win = false;
 function nextturn()
 {
     if (turn==="X")
@@ -24,45 +25,48 @@ function reset()
     document.getElementById("b3").textContent = "_";
     document.getElementById("c3").textContent = "_";
     turn = "X";
+    win = false;
 }
-function win(cell)
-{
-    if ([0, 3, 6].includes(cell) && board[cell]!=null)
+const combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+/*function CheckForWin(array) {
+    if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
     {
-        if (board[cell]==board[cell+1] && board[cell+1]===board[cell+2]) 
-        {
-            alert("win");
-        }
+        return true;
     }
-    else if ([1, 4, 7].includes(cell) && board[cell]!=null)
-    {
-        if (board[cell]==board[cell+1] && board[cell]===board[cell-1])
-        {
-            alert("win");
-        }
-    }
-    else if ([2, 5, 8].includes(cell) && board[cell]!=null)
-    {
-        if (board[cell]==board[cell-1] && board[cell-1]===board[cell-2])
-        {
-            alert("win");
-        }
-    }
-    else if (cell===0) {
-        
-    }
-}
+}*/
 function move(cellid, cell)
 {
-    if (board[cell]==null)
+    if (win===false)
     {
-        board[cell] = turn;
-        document.getElementById(cellid).textContent = turn;
-        win(cell);
-        nextturn();
+        if (board[cell]==null)
+        {
+            board[cell] = turn;
+            document.getElementById(cellid).textContent = turn;
+            combinations.forEach(array => {
+                if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
+                {
+                    alert(turn + " wins!")
+                    win = true;
+                }
+            });
+            nextturn();
+        }
+        else
+        {
+            alert("this cell is already occupied");
+        }
     }
     else
     {
-        alert("this cell is already taken");
+        alert("restart game");
     }
 }
