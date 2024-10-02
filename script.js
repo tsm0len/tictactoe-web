c91ba0811d9c7786406e37dd887c3b38aad80c31
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-02 11:44:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var board = [ , , , , , , , , ];
 var turn = "X";
-function nextturn() {
-    if (turn==="X") {
+function nextturn()
+{
+    if (turn==="X")
+    {
         turn = "O";
-    } else {
+    }
+    else
+    {
         turn = "X";
     }
 }
-function reset() {
+function reset()
+{
     board = [ , , , , , , , , ];
     document.getElementById("a1").textContent = "_";
     document.getElementById("b1").textContent = "_";
@@ -18,14 +23,46 @@ function reset() {
     document.getElementById("a3").textContent = "_";
     document.getElementById("b3").textContent = "_";
     document.getElementById("c3").textContent = "_";
+    turn = "X";
 }
-
-function move(cellid, cell) {
-    if (board[cell]==null) {
+function win(cell)
+{
+    if ([0, 3, 6].includes(cell) && board[cell]!=null)
+    {
+        if (board[cell]==board[cell+1] && board[cell+1]===board[cell+2]) 
+        {
+            alert("win");
+        }
+    }
+    else if ([1, 4, 7].includes(cell) && board[cell]!=null)
+    {
+        if (board[cell]==board[cell+1] && board[cell]===board[cell-1])
+        {
+            alert("win");
+        }
+    }
+    else if ([2, 5, 8].includes(cell) && board[cell]!=null)
+    {
+        if (board[cell]==board[cell-1] && board[cell-1]===board[cell-2])
+        {
+            alert("win");
+        }
+    }
+    else if (cell===0) {
+        
+    }
+}
+function move(cellid, cell)
+{
+    if (board[cell]==null)
+    {
         board[cell] = turn;
         document.getElementById(cellid).textContent = turn;
+        win(cell);
         nextturn();
-    } else {
+    }
+    else
+    {
         alert("this cell is already taken");
     }
 }
