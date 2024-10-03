2ff77368a35f22ab7e60d189f15a6ca3f41d3471
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-03 17:36:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let board = [ , , , , , , , , ];
 let turn = "X";
 let win = false;
+const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
+const combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 function nextturn()
 {
     if (turn==="X")
@@ -14,35 +26,15 @@ function nextturn()
 }
 function reset()
 {
+    document.getElementById("win").style.display = "none";
     board = [ , , , , , , , , ];
-    document.getElementById("a1").textContent = "_";
-    document.getElementById("b1").textContent = "_";
-    document.getElementById("c1").textContent = "_";
-    document.getElementById("a2").textContent = "_";
-    document.getElementById("b2").textContent = "_";
-    document.getElementById("c2").textContent = "_";
-    document.getElementById("a3").textContent = "_";
-    document.getElementById("b3").textContent = "_";
-    document.getElementById("c3").textContent = "_";
+    cellIDs.forEach(id => {
+        document.getElementById(id).textContent = "_";
+        document.getElementById(id).style.backgroundColor = "#e4f1fe";
+    });
     turn = "X";
     win = false;
 }
-const combinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-/*function CheckForWin(array) {
-    if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
-    {
-        return true;
-    }
-}*/
 function move(cellid, cell)
 {
     if (win===false)
@@ -54,19 +46,19 @@ function move(cellid, cell)
             combinations.forEach(array => {
                 if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
                 {
-                    alert(turn + " wins!")
+                    array.forEach(element => {
+                        document.getElementById(cellIDs[element]).style.backgroundColor = "#0044ff";
+                    });
+                    document.getElementById("win").textContent = turn + " WINS";
+                    document.getElementById("win").style.display = "block";
                     win = true;
                 }
             });
             nextturn();
         }
-        else
-        {
-            alert("this cell is already occupied");
-        }
     }
     else
     {
-        alert("restart game");
+        reset();
     }
 }
