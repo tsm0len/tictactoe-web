bb7b03d3954368e4fbeab0babb00deb6fb6af104
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-03 19:27:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let board = [ , , , , , , , , ];
-let turn = "X";
-let win = false;
-const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
-const combinations = [
+let board = [ , , , , , , , , ]; //board state
+let turn = "X"; //current turn
+let win = false; //is current game won or not
+let moves = 0; //number of moves made
+let scores = [0, 0]; //wins for [x, o]
+const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"] //all cell's ids
+const combinations = [ //all win combinations
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -12,8 +14,7 @@ const combinations = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-function nextturn()
+function nextturn() //change turn after move
 {
     if (turn==="X")
     {
@@ -24,7 +25,7 @@ function nextturn()
         turn = "X";
     }
 }
-function reset()
+function reset() //reset game
 {
     document.getElementById("win").style.display = "none";
     board = [ , , , , , , , , ];
@@ -34,14 +35,16 @@ function reset()
     });
     turn = "X";
     win = false;
+    moves = 0
 }
-function move(cellid, cell)
+function move(cellid, cell) //make move in game
 {
     if (win===false)
     {
         if (board[cell]==null)
         {
             board[cell] = turn;
+            moves++;
             document.getElementById(cellid).textContent = turn;
             combinations.forEach(array => {
                 if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
@@ -52,8 +55,20 @@ function move(cellid, cell)
                     document.getElementById("win").textContent = turn + " WINS";
                     document.getElementById("win").style.display = "block";
                     win = true;
+                    if (turn==="X") {
+                        scores[0]++;
+                    } else {
+                        scores[1]++;
+                    }
+                    document.getElementById("scorex").textContent = "X: " + scores[0].toString();
+                    document.getElementById("scoreo").textContent = "O: " + scores[1].toString();
                 }
             });
+            if (moves===9 && win===false) {
+                document.getElementById("win").textContent = "DRAW";
+                document.getElementById("win").style.display = "block";
+                win = true;
+            }
             nextturn();
         }
     }
