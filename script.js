d3b5ad521731bc4ce2f509c9877489d355f02f1a
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-07 16:18:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let turn = "X"; //current turn
 let win = false; //is current game won or not
 let moves = 0; //number of moves made
 let scores = [0, 0, 0]; //wins for [x, o, draw]
-const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"] //all cell's ids
+const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]; //all cell's ids
 const combinations = [ //all win combinations
     [0, 1, 2],
     [3, 4, 5],
@@ -14,8 +14,7 @@ const combinations = [ //all win combinations
     [0, 4, 8],
     [2, 4, 6]
 ];
-function nextturn() //change turn after move
-{
+function nextturn() { //change turn after move
     if (turn==="X")
     {
         turn = "O";
@@ -25,34 +24,29 @@ function nextturn() //change turn after move
         turn = "X";
     }
 }
-function reset() //reset game
-{
+function reset() { //reset game
     document.getElementById("win").style.display = "none";
     board = [ , , , , , , , , ];
     cellIDs.forEach(id => {
         document.getElementById(id).textContent = "_";
         document.getElementById(id).style.backgroundColor = "#e4f1fe";
     });
-    /* document.getElementById("scorex").textContent = "X: 0";
-    document.getElementById("scoredraw").textContent = "DRAW: 0";
-    document.getElementById("scoreo").textContent = "O: 0"; */
     turn = "X";
     win = false;
     moves = 0
-    //scores = [0, 0, 0];
 }
-function move(cellid, cell) //make move in game
-{
-    if (win===false)
-    {
-        if (board[cell]==null)
-        {
+function move(cellid, cell) {
+    place(cellid, cell, true);
+}
+function place(cellid, cell) { //make move in game
+    if (win===false) {
+        if (board[cell]==null) {
             board[cell] = turn;
+            console.log("placed on: "+cell);
             moves++;
             document.getElementById(cellid).textContent = turn;
             combinations.forEach(array => {//================
-                if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null)
-                {
+                if (board[array[0]]===board[array[1]] && board[array[1]]===board[array[2]] && board[array[0]]!=null) {
                     array.forEach(element => {
                         document.getElementById(cellIDs[element]).style.backgroundColor = "#0044ff";
                     });
@@ -65,7 +59,6 @@ function move(cellid, cell) //make move in game
                         scores[1]++;
                     }
                     document.getElementById("scorex").textContent = "X: " + scores[0].toString();
-                    document.getElementById("scoredraw").textContent = "DRAW: " + scores[2].toString();
                     document.getElementById("scoreo").textContent = "O: " + scores[1].toString();
                 }
             });
@@ -73,12 +66,34 @@ function move(cellid, cell) //make move in game
                 document.getElementById("win").textContent = "DRAW";
                 document.getElementById("win").style.display = "block";
                 win = true;
+                scores[2]++;
+                document.getElementById("scoredraw").textContent = "DRAW: " + scores[2].toString();
             }
             nextturn();
+            botmove(choseCell());
         }
-    }
-    else
-    {
+    } else {
         reset();
     }
+}
+function botmove(x) {
+    place(cellIDs[x], x, false);   
+}
+function choseCell() {
+    let chosen = Math.floor(Math.random()*9);
+    //while
+    let testboard = board;
+    for (let i=0;i<9;i++) {
+        if (testboard[i]==null) {
+            testboard[i] = turn;
+            combinations.forEach(array => {
+                if (testboard[array[0]]===testboard[array[1]] && testboard[array[1]]===testboard[array[2]] && testboard[array[0]]!=null) {
+                    chosen = i;
+                    console.log("win on "+i);
+                }
+            });
+            testboard[i] = null;
+        } 
+    }
+    return chosen;
 }
