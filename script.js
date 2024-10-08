64e91fd7ccf2c1b282e54525a90697075bf36628
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-08 19:37:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let turn = "X"; //current turn
 let win = false; //is current game won or not
 let moves = 0; //number of moves made
 let scores = [0, 0, 0]; //wins for [x, o, draw]
+const corners = [0, 2, 6, 8]; 
+const edges = [1, 3, 5, 7]; //const center = 4;
 const cellIDs = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]; //all cell's ids
 const combinations = [ //all win combinations
     [0, 1, 2],
@@ -34,15 +36,17 @@ function reset() { //reset game
     turn = "X";
     win = false;
     moves = 0
+    console.log("cleared");
 }
 function move(cellid, cell) {
     place(cellid, cell, true);
 }
-function place(cellid, cell) { //make move in game
+function place(cellid, cell, byPlayer) { //make move in game
+    //console.log(cellid + " " + cell + " " + byPlayer);
     if (win===false) {
         if (board[cell]==null) {
             board[cell] = turn;
-            console.log("placed on: "+cell);
+            console.log("placed "+turn+" on: "+cell+" | "+byPlayer);
             moves++;
             document.getElementById(cellid).textContent = turn;
             combinations.forEach(array => {//================
@@ -53,6 +57,7 @@ function place(cellid, cell) { //make move in game
                     document.getElementById("win").textContent = turn + " WINS";
                     document.getElementById("win").style.display = "block";
                     win = true;
+                    console.log("won on: "+array+" | "+byPlayer);
                     if (turn==="X") {
                         scores[0]++;
                     } else {
@@ -70,7 +75,9 @@ function place(cellid, cell) { //make move in game
                 document.getElementById("scoredraw").textContent = "DRAW: " + scores[2].toString();
             }
             nextturn();
-            botmove(choseCell());
+            if (byPlayer && !win) {
+                botmove(choseCell());
+            }
         }
     } else {
         reset();
@@ -80,16 +87,50 @@ function botmove(x) {
     place(cellIDs[x], x, false);   
 }
 function choseCell() {
-    let chosen = Math.floor(Math.random()*9);
-    //while
+    let chosen = 4; //Math.floor(Math.random()*9);
+    let emptyCorners = [];
+    let emptyEdges = [];
+    corners.forEach(corner => {
+        if (board[corner]==null) {
+            emptyCorners.push(corner);
+        }
+    });
+    edges.forEach(edge => {
+        if (board[edge]==null) {
+            emptyEdges.push(edge);
+        }
+    });
+    if (board[chosen]!=null) {
+        if (emptyCorners.length) {
+            chosen = emptyCorners[Math.floor(Math.random()*emptyCorners.length)];
+        } else if (emptyEdges.length) {
+            chosen = emptyEdges[Math.floor(Math.random()*emptyEdges.length)];
+        }
+    }
     let testboard = board;
+    nextturn();
     for (let i=0;i<9;i++) {
         if (testboard[i]==null) {
             testboard[i] = turn;
             combinations.forEach(array => {
                 if (testboard[array[0]]===testboard[array[1]] && testboard[array[1]]===testboard[array[2]] && testboard[array[0]]!=null) {
                     chosen = i;
-                    console.log("win on "+i);
+                    console.log("detected loss on "+i);
+                    return chosen;
+                }
+            });
+            testboard[i] = null;
+        } 
+    }
+    nextturn();
+    for (let i=0;i<9;i++) {
+        if (testboard[i]==null) {
+            testboard[i] = turn;
+            combinations.forEach(array => {
+                if (testboard[array[0]]===testboard[array[1]] && testboard[array[1]]===testboard[array[2]] && testboard[array[0]]!=null) {
+                    chosen = i;
+                    console.log("detected win on "+i);
+                    return chosen;
                 }
             });
             testboard[i] = null;
